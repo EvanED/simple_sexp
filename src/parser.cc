606666690d90b1a28e343c621314af4645cfbc30
Evan Driscoll <driscoll@cs.wisc.edu>
2012-08-08 15:30:51 -0500

    Refactoring

--- src/parser.cc
+++ src/parser.cc
@@ -1,15 +1,20 @@
 #include <simple_sexp/sexp.hh>
 
 #include <boost/tokenizer.hpp>
+#include <boost/range/iterator_range.hpp>
 
 #include "lexer.hh"
 
 #include <iostream>
 
-int main()
+using boost::iterator_range;
+using boost::make_iterator_range;
+
+
+template <typename ForwardRange>
+iterator_range<boost::tokenizer<basic_regular_expression_matcher<char>>::iterator>
+get_tokens(ForwardRange const & input)
 {
-    std::string s = "(a b (1 \"foo_bar\" 2))";
-    
     typedef boost::tokenizer<basic_regular_expression_matcher<char> > tokenizer;
 
     basic_regular_expression_matcher<char> matcher;
@@ -22,14 +27,23 @@ int main()
         matcher.add_regex(" +");
     }
     catch (std::exception & e) {
-        std::cout << "Exception: " << e.what() << "\n";
+        std::cout << "Internal error 398928: exception: " << e.what() << "\n";
         throw;
     }
     
-    tokenizer tok(s.begin(), s.end(), matcher);
-    
-    for(tokenizer::iterator beg=tok.begin(); beg!=tok.end();++beg){
-        std::cout << *beg << "\n";
+    tokenizer tok(std::begin(input), std::end(input), matcher);
+
+    return make_iterator_range(tok.begin(), tok.end());
+}
+
+
+int main()
+{
+    std::string s = "(a b (1 \"foo_bar\" 2))";
+
+    auto range = get_tokens(s);
+    for(auto token : range) {
+        std::cout << token << "\n";
     }
 }
 
